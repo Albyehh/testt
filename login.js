868d1ea0a8d49f02d2dda1c6d529ca145904ea13
Albyehh <116997472+Albyehh@users.noreply.github.com>
2022-12-04 21:49:10 +0800

    Add files via upload

---
+++ login.js
@@ -0,0 +1,10 @@
+function validate(){
+    var password = document.getElementById("password").value;
+    if (password == "cikeyencantik"){
+    window.location='letter1.html';
+    return false;
+    }
+    else{
+        alert("klo gtw kode ny gsh kepo y");
+    }
+}
